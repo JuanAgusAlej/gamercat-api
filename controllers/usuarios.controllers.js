50dedb3f82a04b3469282b3c657019979144b871
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-05-27 01:17:38 -0300

    comenzando a trabajar en el modelo de usuario

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,46 +1,47 @@
-const {response, request} = require('express');
+const { response, request } = require("express");
 
 const usuariosGet = (req = request, res = response) => {
-    
-    const query = req.query;//toma las query que se envio en la ruta
-    const {limit=5, size=5, page=1}= req.query;//toma las query que se envio en la ruta
+  //   const query = req.query; //toma las query que se envio en la ruta
+  const { limit = 5, nombre = "Sin Nombre", apikey, page = 1 } = req.query; //toma las query que se envio en la ruta
 
-
-    res.status(201).json({
-        msg: 'get: mostar informacion',
-        limit,
-        size,
-        page,
-    });
-}
+  res.status(201).json({
+    msg: "get: mostar informacion",
+    limit,
+    nombre,
+    apikey,
+    page,
+  });
+};
 
 const usuariosPost = (req = request, res = response) => {
-    const dato = req.body;//toma los datos que se envian
+  const dato = req.body; //toma los datos que se envian
 
-    res.json({
-        msg: 'post: crear informacion',
-        dato
-    });
-}
+  res.json({
+    msg: "post: crear informacion",
+    dato,
+  });
+};
 
 const usuariosPut = (req = request, res = response) => {
-    
-    const id = req.params.id;//toma el parametro que se envio en la ruta
+  const id = req.params.id; //toma el parametro que se envio en la ruta
 
-    res.json({
-        msg: 'put: actualizar informacion'
-    });
-}
+  res.json({
+    msg: "put: actualizar informacion",
+    id,
+  });
+};
 
-const usuariosDelete =(req=request, res=response) => {
-    res.json({
-        msg: 'delete: eliminar informacion'
-    });
-}
+const usuariosDelete = (req = request, res = response) => {
+  const id = req.params.id;
+  res.json({
+    msg: "delete: eliminar informacion",
+    id,
+  });
+};
 
 module.exports = {
-    usuariosGet,
-    usuariosPost,
-    usuariosPut,
-    usuariosDelete
-}
+  usuariosGet,
+  usuariosPost,
+  usuariosPut,
+  usuariosDelete,
+};
